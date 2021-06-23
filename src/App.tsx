17eb9f6a3b2d96324ac590d251b5862d6964b179
cipriano99 <samuelcipriano99@gmail.com>
2021-06-22 23:52:46 -0300

    Improve the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }

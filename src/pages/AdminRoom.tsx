9035f9fc0227e32bf7bcf6abcd0af8d16dc13c48
cipriano99 <samuelcipriano99@gmail.com>
2021-06-25 10:47:08 -0300

    add Delete question button feature

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router';
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
@@ -8,6 +9,7 @@ import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
 
 import '../styles/room.css';
+import { database } from '../services/firebase';
 
 type RoomParams = {
   id: string;
@@ -19,6 +21,12 @@ export function AdminRoom() {
   const roomId = params.id;
 
   const { questions, title } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Tem certeza que você quer remover esta pergunta?!')) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -45,7 +53,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
